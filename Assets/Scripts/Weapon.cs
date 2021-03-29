2b7f23f7133a64e70a5516f3d5736a756e6cbd06
Xellinus <spraguechristopher@gmail.com>
2021-03-28 18:41:34 -0700

    predin push

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    public float damage;
+    public float speed;
+    public float range;
+
+}
